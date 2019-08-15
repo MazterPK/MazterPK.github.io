abf6b76ebd886ad3898840aee109a5cd6e2c09f4
MazterPK <petekanyuk@gmail.com>
2019-08-14 22:44:49 -0400

    Add files via upload

--- Website/dateScript.js
+++ Website/dateScript.js
@@ -20,7 +20,11 @@ function generate() {
     var race = genRace();
     genStats(clas, race);
     genName(race);
+    genSkills(clas, race);
     
+}
+function genSkills(clas, race) {
+
 }
 function clearName() {
     document.getElementById("charLb").value = "";
@@ -133,54 +137,85 @@ function genStats(clas, race) {
             int += 1;
             break;
     }
+    var strSv = 0;
+    var dexSv = 0;
+    var conSv = 0;
+    var intSv = 0;
+    var wisSv = 0;
+    var chaSv = 0;
+
     switch (clas) {
         case "Barbarian":
             document.getElementById("strSvChk").checked = true;
             document.getElementById("conSvChk").checked = true;
+            strSv = 2;
+            conSv = 2;
             break;
         case "Bard":
             document.getElementById("dexSvChk").checked = true;
             document.getElementById("chaSvChk").checked = true;
+            dexSv = 2;
+            chaSv = 2;
             break;
         case "Cleric":
             document.getElementById("wisSvChk").checked = true;
             document.getElementById("chaSvChk").checked = true;
+            wisSv = 2;
+            chaSv = 2;
             break;
         case "Druid":
             document.getElementById("wisSvChk").checked = true;
             document.getElementById("intSvChk").checked = true;
+            wisSv = 2;
+            intSv = 2;
             break;
         case "Fighter":
             document.getElementById("strSvChk").checked = true;
             document.getElementById("conSvChk").checked = true;
+            strSv = 2;
+            conSv = 2;
             break;
         case "Monk":
             document.getElementById("strSvChk").checked = true;
             document.getElementById("dexSvChk").checked = true;
+            strSv = 2;
+            dexSv = 2;
             break;
         case "Paladin":
             document.getElementById("wisSvChk").checked = true;
             document.getElementById("chaSvChk").checked = true;
+            wisSv = 2;
+            chaSv = 2;
             break;
         case "Ranger":
             document.getElementById("strSvChk").checked = true;
             document.getElementById("dexSvChk").checked = true;
+            strSv = 2;
+            dexSv = 2;
             break;
         case "Rogue":
             document.getElementById("dexSvChk").checked = true;
             document.getElementById("intSvChk").checked = true;
+            dexSv = 2;
+            intSv = 2;
             break;
         case "Sorcerer":
             document.getElementById("conSvChk").checked = true;
             document.getElementById("chaSvChk").checked = true;
+            conSv = 2;
+            chaSv = 2;
             break;
         case "Warlock":
             document.getElementById("wisSvChk").checked = true;
             document.getElementById("chaSvChk").checked = true;
+            wisSv = 2;
+            chaSv = 2;
             break;
         case "Wizard":
             document.getElementById("intSvChk").checked = true;
             document.getElementById("wisSvChk").checked = true;
+            intSv = 2;
+            wisSv = 2;
             break;
     }
     var strMod = getMod(str);
@@ -189,18 +224,34 @@ function genStats(clas, race) {
     var wisMod = getMod(wis);
     var intMod = getMod(int);
     var chaMod = getMod(cha);
+
+    strSv += strMod;
+    dexSv += dexMod;
+    conSv += conMod;
+    intSv += intMod;
+    wisSv += wisMod;
+    chaSv += chaMod;
+
     document.getElementById("strLb").value = str;
     document.getElementById("dexLb").value = dex;
     document.getElementById("conLb").value = con;
     document.getElementById("wisLb").value = wis;
     document.getElementById("intLb").value = int;
     document.getElementById("chaLb").value = cha;
+
     document.getElementById("strmodLb").value = strMod;
     document.getElementById("dexmodLb").value = dexMod;
     document.getElementById("conmodLb").value = conMod;
     document.getElementById("wismodLb").value = wisMod;
     document.getElementById("intmodLb").value = intMod;
     document.getElementById("chamodLb").value = chaMod;
+
+    document.getElementById("strSvMod").value = strSv;
+    document.getElementById("dexSvMod").value = dexSv;
+    document.getElementById("conSvMod").value = conSv;
+    document.getElementById("intSvMod").value = intSv;
+    document.getElementById("wisSvMod").value = wisSv;
+    document.getElementById("chaSvMod").value = chaSv;
 }
 function roll(numDice, numSides) {
     var temp = 0;
